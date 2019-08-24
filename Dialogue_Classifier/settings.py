4d1e99eb49be3560371fa793d387b6c6eb01c90f
Max Borch <max.borch.18@ucl.ac.uk>
2019-08-24 12:22:59 +0100

    alles geht wieder

--- Dialogue_Classifier/settings.py
+++ Dialogue_Classifier/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_ROOT = ''
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join('static'),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
